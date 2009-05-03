d3b49024cc23e641b7123e2d067be760997a15d6
springmeyer dane springmeyer <springmeyer dane.springmeyer@gmail.com>
2009-05-03 15:44:07 -0700

    touchup for first release

--- deploy.py
+++ deploy.py
@@ -1,14 +1,17 @@
+
 #!/usr/bin/env python
 
 """
 Release Steps
 -------------
+ * write 'setup.py' file
  * create ~./pypirc file with pypi user:pass
+ * register package with: 'python setup.py register'
  * Edit CHANGELOG.txt
  * Increment '__version__' in main script and 'version' in setup.py
  * Run `deploy.py` to create sdist, upload, and create tag
  * Commit tag
- * Update Google Code wiki
+ * Update wiki
 """
 
 import sys
@@ -32,18 +35,15 @@ def cleanup():
     call('sudo rm *.egg* *.pyc dist/ build/ -r -f')
 
 def tag():
-    call('svn cp ../%s/ %s/%s' % (app,tag_dir,version))
-    call('svn rm %s/%s/deploy.py' % (tag_dir,version))
+    call('hg tag -u springmeyer %s -m "tag %s release" ' % (version,version))
 
-def package():
+def upload():
     call('python setup.py sdist upload')
-    #call('cp dist/%s-%s.tar.gz %s' % (app,version,tag_dir))
-    #call('svn add %s/%s-%s.tar.gz' % (tag_dir,app,version))
 
 def main():
     cleanup()
-    #tag()
-    #package()
+    tag()
+    upload()
     cleanup()
     
 if __name__ = '__main__':
--- tilelite.py
+++ tilelite.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
-__author__ = "Dane Springmeyer (dbsgeo [ -a- ] gmail.com)"
-__copyright__ = "Copyright 2009, Dane Springmeyer"
-__version__ = "0.1.0"
-__license__ = "BSD"
+__author__ = 'Dane Springmeyer (dbsgeo [ -a- ] gmail.com)'
+__copyright__ = 'Copyright 2009, Dane Springmeyer'
+__version__ = '0.1.0'
+__license__ = 'BSD'
 
 from sys import stderr
 from time import sleep
