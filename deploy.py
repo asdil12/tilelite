df0af7cde01feb2924b5404a99eb51503785a31c
springmeyer dane springmeyer <springmeyer dane.springmeyer@gmail.com>
2009-05-03 12:56:23 -0700

    prepare for first release (0.1.0)

---
+++ deploy.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+"""
+Release Steps
+-------------
+ * create ~./pypirc file with pypi user:pass
+ * Edit CHANGELOG.txt
+ * Increment '__version__' in main script and 'version' in setup.py
+ * Run `deploy.py` to create sdist, upload, and create tag
+ * Commit tag
+ * Update Google Code wiki
+"""
+
+import sys
+import time
+from subprocess import call as subcall
+
+app = 'tilelite'
+version = __import__(app).__version__
+
+def call(cmd):
+  try:
+    response = subcall(cmd,shell=True)
+    print
+    time.sleep(1)
+    if response < 0:
+      sys.exit(response)
+  except OSError, E:
+    sys.exit(E)
+
+def cleanup():
+    call('sudo rm *.egg* *.pyc dist/ build/ -r -f')
+
+def tag():
+    call('svn cp ../%s/ %s/%s' % (app,tag_dir,version))
+    call('svn rm %s/%s/deploy.py' % (tag_dir,version))
+
+def package():
+    call('python setup.py sdist upload')
+    #call('cp dist/%s-%s.tar.gz %s' % (app,version,tag_dir))
+    #call('svn add %s/%s-%s.tar.gz' % (tag_dir,app,version))
+
+def main():
+    cleanup()
+    #tag()
+    #package()
+    cleanup()
+    
+if __name__ = '__main__':
+    main()
--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from distutils.core import setup
 
-version = '0.1'
+version = '0.1.0'
 app = 'tilelite'
-description = 'Lightweight WSGI tile-server, written in Python, using Mapnik rendering and designed to serve OSM (OpenStreetMap) tiles.'
+description = 'Lightweight WSGI tile-server, written in Python, using Mapnik rendering and designed to serve tiles in the OSM/Google scheme.'
 url = 'http://bitbucket.org/springmeyer/%s/' % app
+readme = file('README.txt','rb').read()
 
 setup(name='%s' % app,
       version=version,
       description=description,
+      #long_description=readme,
       author='Dane Springmeyer',
       author_email='dbsgeo@gmail.com',
       requires=['Mapnik'],
@@ -18,7 +20,7 @@ setup(name='%s' % app,
       py_modules=['%s' % app],
       #packages=['%s' % app],
       scripts = ['liteserv.py'],
-      classifiers=['Development Status :: 3 - Alpha',
+      classifiers=['Development Status :: 4 - Beta',
                    'Environment :: Web Environment',
                    'Framework :: Django',
                    'Intended Audience :: Developers',
